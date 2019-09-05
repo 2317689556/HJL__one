26d4a14ae40dad1f22231226bc5df620b7f1bd9d
your name <your email>
2019-09-05 17:00:23 +0800

    HJL

---
+++ src/main/java/com/buba/boot1806a/dao/sbMapper.java
@@ -0,0 +1,19 @@
+package com.buba.boot1806a.dao;
+
+import com.buba.boot1806a.pojo.Plan;
+import com.buba.boot1806a.pojo.tbDept;
+
+import java.util.List;
+
+public interface sbMapper {
+
+
+    /*预报数据的提交*/
+    int addPlan(Plan p);
+
+    /*根据id单查*/
+    tbDept findDeptById(Integer deptId);
+
+    /*分页展示上报数据*/
+    List<Plan> findPlan(Integer deptId);
+}
---
+++ src/main/java/com/buba/boot1806a/service/sbService.java
@@ -0,0 +1,12 @@
+package com.buba.boot1806a.service;
+
+import com.buba.boot1806a.pojo.Plan;
+import com.github.pagehelper.PageInfo;
+
+public interface sbService {
+    /*预报数据的提交*/
+    int addPlan(Plan p);
+
+    /*分页展示上报数据*/
+    PageInfo<Plan> findPlan(Integer deptId, String pageNum, String defaultPageSize);
+}
